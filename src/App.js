e20d5aa997f1abc0870af88d10556cbc6cdfbd24
Tom Officer <thomasofficerjr@gmail.com>
2021-10-04 13:59:18 -0700

    New Layout added, css stylings, show comments button

--- src/App.js
+++ src/App.js
@@ -1,34 +1,67 @@
-import './App.css'
 import { useEffect, useState } from 'react'
-import { GetComments } from './GetComments';
+import "./App.css"
+import { GetComments } from './GetComments'
 
-export default function App() {
+function App() {
 
-  //declare posts variable, set default state to empty array
-  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
 
-  //initial fetch call for all posts data from userId 7, can be any number/param here, i chose 7
-  //set dependecy to an empty array, hence only making the fetch call once up initial page load
   useEffect(() => {
-    const url = 'https://jsonplaceholder.typicode.com/posts?userId=7';
+    const url = 'https://jsonplaceholder.typicode.com/posts'
 
     fetch(url)
       .then(res => res.json())
-      .then(data => setPosts(data))
+      .then(data => setAllPosts(data))
       .catch(err => console.log(err))
   }, []);
 
-  //variable to store main render in App component, which holds ALL data, including comments and has clickability from GetComments component, pass post in as props to GetComments component
-  const mainDisplay = posts.map(post => (
+  const mainRender = allPosts.map(post => (
     <div>
-      <GetComments post={post} />
+      <GetComments post={post}/>
     </div>
-  ));
+  ))
 
   return (
     <div className="App">
-      {mainDisplay}
+      {mainRender}
     </div>
-  );
+  )
 }
 
+export default App;
+
+
+
+
+
+
+
+
+
+
+//   //declare posts variable, set default state to empty array
+//   const [posts, setPosts] = useState([]);
+
+//   //initial fetch call for all posts data from userId 7, can be any number/param here, i chose 7
+//   //set dependecy to an empty array, hence only making the fetch call once up initial page load
+//   useEffect(() => {
+//     const url = 'https://jsonplaceholder.typicode.com/posts?userId=7';
+
+//     fetch(url)
+//       .then(res => res.json())
+//       .then(data => setPosts(data))
+//       .catch(err => console.log(err))
+//   }, []);
+
+//   //variable to store main render in App component, which holds ALL data, including comments and has clickability from GetComments component, pass post in as props to GetComments component
+//   const mainDisplay = posts.map(post => (
+//     <div>
+//       <GetComments post={post} />
+//     </div>
+//   ));
+
+//   return (
+//     <div className="App">
+//       {mainDisplay}
+//     </div>
+//   );
--- src/GetComments.js
+++ src/GetComments.js
@@ -1,44 +1,39 @@
 import { useEffect, useState } from 'react'
 
 
-
 export const GetComments = ({ post }) => {
 
-    //variable for show state, default to false/not showing
-    const [show, setShow] = useState(false);
+    const [allComments, setAllComments] = useState([]);
+    const [showComments, setShowComments] = useState(false);
 
-    //variable for comments state, defaults to empty array
-    const [comments, setComments] = useState([]);
-
-    //second fetch call to retrieve data from comments for specified user
-    //set dependecy to specific id pertaining to EACH post, the post.id is specific to each post so we use [post.id] which will make this fetch call for every post within the specified userId
     useEffect(() => {
         const url = `https://jsonplaceholder.typicode.com/posts/${post.id}/comments`;
 
         fetch(url)
             .then(res => res.json())
-            .then(data => setComments(data))
-            .catch(err => console.log(err))
+            .then(data => setAllComments(data))
+        .catch(err => console.log(err)) 
     }, [post.id]);
-
-    //click handler for post.body to show comments upon click
-    const clickHandler = () => {
-        setShow(!show);
+    
+    const displayComments = () => {
+        setShowComments(!showComments)
     }
 
     return (
-        <div>
-            <h2>{post.title}</h2>
-            <p >{post.body}</p>
-            <button onClick={clickHandler}>Show {post.title} Comments</button>
-            {
-                show &&
-                    <div>
-                    {comments.map(comment => (
-                    <div>
-                        <h6>Comments: {comment.body}</h6>
-                    </div>
-                    ))}
+        <div className="App">
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+            <button onClick={displayComments}>Show Comments For This Post?</button>
+            {showComments && 
+            
+                <div>
+                {
+                    allComments.map(comment => (
+                        <div>
+                            {comment.body}
+                        </div>
+                    ))
+                }
                 </div>
             }
         </div>
